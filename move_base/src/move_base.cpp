ed86bc3871c1b14a1ef7717b0e2580c786f5d3d0
msb336 <msb336@drexel.edu>
2019-02-21 16:45:41 -0800

    changed move-base

--- move_base/src/move_base.cpp
+++ move_base/src/move_base.cpp
@@ -54,7 +54,6 @@ namespace move_base {
     recovery_loader_("nav_core", "nav_core::RecoveryBehavior"),
     planner_plan_(NULL), latest_plan_(NULL), controller_plan_(NULL),
     runPlanner_(false), setup_(false), p_freq_change_(false), c_freq_change_(false), new_global_plan_(false) {
-      ROS_INFO("INITIALIZING MOVE_BASE");
     as_ = new MoveBaseActionServer(ros::NodeHandle(), "move_base", boost::bind(&MoveBase::executeCb, this, _1), false);
 
     ros::NodeHandle private_nh("~");
@@ -62,7 +61,6 @@ namespace move_base {
 
     recovery_trigger_ = PLANNING_R;
 
-    ROS_INFO("getting some parameters");
     //get some parameters that will be global to the move base node
     std::string global_planner, local_planner;
     private_nh.param("base_global_planner", global_planner, std::string("navfn/NavfnROS"));
@@ -99,7 +97,7 @@ namespace move_base {
     ros::NodeHandle simple_nh("move_base_simple");
     goal_sub_ = simple_nh.subscribe<geometry_msgs::PoseStamped>("goal", 1, boost::bind(&MoveBase::goalCB, this, _1));
 
-    ROS_INFO("getting robot parameters");
+    // ROS_INFO("getting robot parameters");
     //we'll assume the radius of the robot to be consistent with what's specified for the costmaps
     private_nh.param("local_costmap/inscribed_radius", inscribed_radius_, 0.325);
     private_nh.param("local_costmap/circumscribed_radius", circumscribed_radius_, 0.46);
@@ -110,7 +108,7 @@ namespace move_base {
     private_nh.param("clearing_rotation_allowed", clearing_rotation_allowed_, true);
     private_nh.param("recovery_behavior_enabled", recovery_behavior_enabled_, true);
 
-    ROS_INFO("getting and pausing global_costmap");
+    // ROS_INFO("getting and pausing global_costmap");
     //create the ros wrapper for the planner's costmap... and initializer a pointer we'll use with the underlying map
     planner_costmap_ros_ = new costmap_2d::Costmap2DROS("global_costmap", tf_);
     planner_costmap_ros_->pause();
@@ -118,7 +116,7 @@ namespace move_base {
     
     //initialize the global planner
     try {
-      ROS_INFO("initializing the global planner");
+      // ROS_INFO("initializing the global planner");
       planner_ = bgp_loader_.createInstance(global_planner);
       planner_->initialize(bgp_loader_.getName(global_planner), planner_costmap_ros_);
     } catch (const pluginlib::PluginlibException& ex) {
@@ -140,7 +138,6 @@ namespace move_base {
       exit(1);
     }
 
-    ROS_INFO("staring costmap");
     // Start actively updating costmaps based on sensor data
     planner_costmap_ros_->start();
     controller_costmap_ros_->start();
@@ -644,7 +641,6 @@ namespace move_base {
 
   void MoveBase::executeCb(const move_base_msgs::MoveBaseGoalConstPtr& move_base_goal)
   {
-    ROS_INFO("move_base: executeCb");
     if(!isQuaternionValid(move_base_goal->target_pose.pose.orientation)){
       as_->setAborted(move_base_msgs::MoveBaseResult(), "Aborting on goal because it was sent with an invalid quaternion");
       return;
